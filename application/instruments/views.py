f491bebfdf92f246df1ae3e80a24c6cdb8dd3b0d
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-24 09:08:32 +0200

    User can delete and update instrument

--- application/instruments/views.py
+++ application/instruments/views.py
@@ -10,13 +10,23 @@ def instruments_index():
 def instruments_form():
     return render_template("instruments/new.html")
 
+@app.route("/instruments/<instrument_id>/delete/", methods=["POST"])
+def instruments_delete(instrument_id):
+    instru = Instrument.query.get(instrument_id)
+    db.session().delete(instru)
+    db.session().commit()
+    return redirect(url_for("instruments_index"))
+    
+@app.route("/instruments/<instrument_id>/", methods=["POST"])
+def instruments_update_name(instrument_id):
+    instru = Instrument.query.get(instrument_id)
+    instru.name = request.form.get("name")
+    db.session().commit()  
+    return redirect(url_for("instruments_index"))
+
 @app.route("/instruments/", methods=["POST"])
 def instruments_create():
-    print(request.form.get("name"))
-    
     instr = Instrument(request.form.get("name"))
-    
     db.session().add(instr)
     db.session().commit()
-
     return redirect(url_for("instruments_index"))
