c1b758c4b9a6a38a1d9eb818b51ccf2b30caa380
Juha Malinen <juha.malinen@helsinki.fi>
2020-02-07 23:57:11 +0200

    Heroku settings

--- application/absence/forms.py
+++ application/absence/forms.py
@@ -4,7 +4,7 @@ from wtforms import DateField, StringField, TextField, validators
 
 class AbsenceForm(FlaskForm):
     name = StringField("Absence name", [validators.Length(min=2)])
-    description = TextField("Absence description")
+    description = StringField("Absence description")
     date_start = DateField('Start date YYYY-MM-DD', format='%Y-%m-%d')
     date_end = DateField('End date YYYY-MM-DD', format='%Y-%m-%d')
 
--- application/event/forms.py
+++ application/event/forms.py
@@ -4,7 +4,7 @@ from wtforms import DateField, StringField, TextField, validators
 
 class EventForm(FlaskForm):
     name = StringField("Event name", [validators.Length(min=2)])
-    description = TextField("Event description")
+    description = StringField("Event description")
     date_start = DateField('Start date YYYY-MM-DD', format='%Y-%m-%d')
     date_end = DateField('End date YYYY-MM-DD', format='%Y-%m-%d')
 
