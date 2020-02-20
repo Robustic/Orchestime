ba34fb65c30db309788a0fcd4ad2d00e5aabbda2
Juha Malinen <juha.malinen@helsinki.fi>
2020-02-20 11:50:52 +0200

    SQL query updated

--- application/event/models.py
+++ application/event/models.py
@@ -35,13 +35,13 @@ class Event(Base):
 
     @staticmethod
     def find_events():
-        stmt = text("SELECT Event.name as event_name,"
-                    " Event.description as event_description,"
-                    " Event.date_start as event_date_start,"
-                    " Event.date_end as event_date_end,"
-                    " Event.id as event_id,"
-                    " Place.name as place_name,"
-                    " Room.name as room_name,"
+        stmt = text("SELECT Event.name AS event_name,"
+                    " Event.description AS event_description,"
+                    " Event.date_start AS event_date_start,"
+                    " Event.date_end AS event_date_end,"
+                    " Event.id AS event_id,"
+                    " Room.name AS room_name,"
+                    " Place.name AS place_name,"                    
                     " COUNT(DISTINCT Account.id) as count_names"
                     " FROM Event"
                     " LEFT JOIN absence_event ON Event.id = absence_event.event_id"
@@ -49,17 +49,17 @@ class Event(Base):
                     " LEFT JOIN account ON account.id = Absence.account_id"
                     " LEFT JOIN Room ON Room.id = Event.room_id"
                     " LEFT JOIN Place ON Place.id = Room.place_id"
-                    " GROUP BY Event.id"
+                    " GROUP BY Event.id, Room.name, Place.name"
                     " ORDER BY event_name")
         return db.engine.execute(stmt)
 
     @staticmethod
     def find_absent_users_for_event(event_id):
-        stmt = text("SELECT account.name as account_name,"
-                    " Absence.name as absence_name,"
-                    " Absence.description as absence_description,"
-                    " Absence.date_start as absence_date_start,"
-                    " Absence.date_end as absence_date_end"
+        stmt = text("SELECT account.name AS account_name,"
+                    " Absence.name AS absence_name,"
+                    " Absence.description AS absence_description,"
+                    " Absence.date_start AS absence_date_start,"
+                    " Absence.date_end AS absence_date_end"
                     " FROM Event"
                     " LEFT JOIN absence_event ON Event.id = absence_event.event_id"
                     " LEFT JOIN Absence ON Absence.id = absence_event.absence_id"
@@ -71,13 +71,13 @@ class Event(Base):
 
     @staticmethod
     def find_all_absents_for_user(user_id):
-        stmt = text("SELECT Event.name as event_name,"
-                    " Event.date_start as event_date_start,"
-                    " Event.date_end as event_date_end,"
-                    " Absence.name as absence_name,"
-                    " Absence.description as absence_description,"
-                    " Absence.date_start as absence_date_start,"
-                    " Absence.date_end as absence_date_end"
+        stmt = text("SELECT Event.name AS event_name,"
+                    " Event.date_start AS event_date_start,"
+                    " Event.date_end AS event_date_end,"
+                    " Absence.name AS absence_name,"
+                    " Absence.description AS absence_description,"
+                    " Absence.date_start AS absence_date_start,"
+                    " Absence.date_end AS absence_date_end"
                     " FROM Absence"
                     " LEFT JOIN absence_event ON Absence.id = absence_event.absence_id"
                     " LEFT JOIN Event ON Event.id = absence_event.event_id"                    
